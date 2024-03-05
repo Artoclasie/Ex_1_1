575b4dc2389ba93df29b62d8b034ce062e7828d6
PC <PC@LAPTOP-QVI7ULCL>
2024-03-05 20:41:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex_1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            
+            if (1 <= x && x <= 3) 
+            {
+                Console.WriteLine($"{2*Math.Pow(x,2) - 3*Math.Pow(Math.E,Math.Sin(x))}");
+            }
+            if (x>3)
+            {
+                Console.WriteLine($"{3*Math.Tan(x)}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка");
+            }
+        }
+    }
+}
